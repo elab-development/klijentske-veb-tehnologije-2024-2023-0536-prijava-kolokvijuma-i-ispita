51f32732c104b5bd1692ba2c3b2dfbc0ecb25dbe
st20230536@student.fon.bg.ac.rs <bojanarajic07@gmail.com>
2025-09-25 20:57:53 +0100

    Route protector

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Exams from './pages/Exams';
 import MyRegistrations from './pages/MyRegistrations';
 import NotFound from './pages/NotFound';
+import { AuthProvider } from './context/AuthContext';
+import RequireAuth from './components/RequireAuth';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login />} />
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login />} />
 
-        <Route element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='/exams' element={<Exams />} />
-          <Route path='/my-registrations' element={<MyRegistrations />} />
-        </Route>
+          <Route element={<RequireAuth />}>
+            <Route element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path='/exams' element={<Exams />} />
+              <Route path='/my-registrations' element={<MyRegistrations />} />
+            </Route>
+          </Route>
 
-        <Route path='/home' element={<Navigate to='/' replace />} />
-
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
